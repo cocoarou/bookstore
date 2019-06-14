fbf7976d013f61596330fdd1d1640fafe83f5203
l1l <manuel.pulcinelli.wow@gmail.com>
2019-06-14 12:09:34 +0200

    _

--- bookstore/src/main/java/it/key2/formazione/bookstore/validators/PasswordMatchesValidator.java
+++ bookstore/src/main/java/it/key2/formazione/bookstore/validators/PasswordMatchesValidator.java
@@ -29,25 +29,9 @@ public class PasswordMatchesValidator implements ConstraintValidator<PasswordMat
     @Override
     public boolean isValid(Object obj, ConstraintValidatorContext context) {
         User user = (User) obj; 
-<<<<<<< HEAD
         if(userRepository.findByUsername(user.getUsername()).isPresent()){
             User userr = userRepository.findByUsername(user.getUsername()).get();
             String psw = userr.getPassword();
-=======
-<<<<<<< HEAD
-        if(userRepository.findByUsername(user.getUsername()).isPresent()) {
-            String psw = userRepository.findByUsername(user.getUsername()).get().getPassword();
-=======
-<<<<<<< HEAD
-        if(userRepository.findByUsername(user.getUsername()).isPresent()) {
-            String psw = userRepository.findByUsername(user.getUsername()).get().getPassword();
-=======
-        if(userRepository.findByUsername(user.getUsername()).isPresent()){
-            User userr = userRepository.findByUsername(user.getUsername()).get();
-            String psw = userr.getPassword();
->>>>>>> testSecurity
->>>>>>> a955ac3b21d945a7bc498195ca767c2fdaa3329e
->>>>>>> 94228f27bb15d39dd9841d7e5ebfd29a1a59a652
             return encryptUtils.matches(user.getPassword(), psw);
         } else {
             return false;
